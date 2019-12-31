9eae220ab3803b2671e800729db3ea57483cfdf9
iberno <iberno@outlook.com>
2019-12-31 09:00:16 -0300

    Router Module Config

--- server.js
+++ server.js
@@ -1,10 +1,16 @@
 const express = require('express');
 const dotenv = require('dotenv');
 
+// Route File
+const bootcamps = require('./routes/bootcamps');
+
 //Load env variables
 dotenv.config({ path: './config/config.env' });
 
 const app = express();
+
+// Mount Routes
+app.use('/api/v1/bootcamps', bootcamps);
 
 const PORT = process.env.PORT || 5000;
 
