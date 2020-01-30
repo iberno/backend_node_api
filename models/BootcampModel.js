aef380462f606b325ac3f954191e123a468eb47b
Iberno <iberno@outlook.com>
2020-01-30 20:15:46 -0300

    Implements the location Map using MapQuest

--- models/BootcampModel.js
+++ models/BootcampModel.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const geocoder = require('../utils/geocoder');
 const slugify = require('slugify');
 
 const BootcampSchema =  new mongoose.Schema({
@@ -39,11 +40,9 @@ const BootcampSchema =  new mongoose.Schema({
     type: {
       type: String,
       enum: ['Point'],
-      required: false
     },
     coordinates: {
       type: [Number],
-      required: false,
       index: '2dsphere',
     },
     formattedAddress: String,
@@ -103,5 +102,23 @@ BootcampSchema.pre('save', function(next) {
   next();
 });
 
+//Geocode & create location field
+BootcampSchema.pre('save', async function(next) {
+  const loc = await geocoder.geocode(this.address);
+  this.location = {
+    type: 'Point',
+    coordinates: [loc[0].longitude, loc[0].latitude],
+    formattedAddress: loc[0].formattedAddress,
+    street: loc[0].streetName,
+    city: loc[0].city,
+    state: loc[0].state,
+    zipcode: loc[0].zipcode,
+    country: loc[0].countryCode
+  };
+
+  //Do not save address in DB
+  this.address = undefined;
+  next();
+});
 
 module.exports = mongoose.model('Bootcamp', BootcampSchema);
