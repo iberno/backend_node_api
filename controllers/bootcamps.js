f6b49475e83de3a8a88503debdf4cf36ec499367
Iberno <iberno@outlook.com>
2020-01-31 15:03:19 -0300

    more about querys

--- controllers/bootcamps.js
+++ controllers/bootcamps.js
@@ -8,13 +8,37 @@ const Bootcamp = require('../models/BootcampModel');
 // @access  Public
 exports.getBootcamps =  asyncHandler(async (req, res, next) => {
   let query;
+  // Copy req.query
+  const reqQuery = { ...req.query}
 
-  let queryStr = JSON.stringify(req.query);
+  // select 
+  const removeFields = ['select', 'sort'];
+  // Fields to exclude 
+  removeFields.forEach(param => delete reqQuery[param]);
 
+  // Create query string
+  let queryStr = JSON.stringify(reqQuery);
+  // Create operators ($gt, $gte, etc)
   queryStr = queryStr.replace(/\b(gt|gte|lt|lte|in)\b/g, match => `$${match}`);
   
+  //Find resource
   query = Bootcamp.find(JSON.parse(queryStr));
+  
+  // Select fields
+  if(req.query.select) {
+    const fields = req.query.select.split(',').join(' ');
+    query = query.select(fields);
+  }
 
+  // Sort
+  if(req.query.sort) {
+    const sortBy = req.query.sort.split(',').join(' ');
+    query = query.sort(sortBy);
+  } else {
+    query = query.sort('-createdAt');
+  }
+
+  // Executing Query
   const bootcamps = await query;
 
   res.status(200).json({
